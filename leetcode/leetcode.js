7a2e859bc17397ec375a4038e6ca630535b0ab96
colewendling <colewendling@utexas.edu>
2021-08-10 10:11:36 -0500

    Leetcode - Throtteling Gateway Problem

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// 1. short fizzbuzz
+// 1. short fizzbuzz.
 const fizzBuzz = function (n) {
   for (let i = 1; i <= n; i++) {
     if (i % 15 == 0) console.log('FizzBuzz');
@@ -9,8 +9,9 @@ const fizzBuzz = function (n) {
     else console.log(i);
   }
 }
+// console.log(fizzBuzz(15));
 
-// 2. scoresArr -> ranks -> # of players pass cutoff rank
+// 2. leetcode - scoresArr -> ranks -> # of players pass cutoff rank.
 
 function numPlayers(k, scores) {
   const sortedScores = scores.sort(function (a, b) {
@@ -26,5 +27,27 @@ function numPlayers(k, scores) {
   const result = ranks.filter((rank) => rank <= k);
   return result.length;
 }
-
 // console.log(numPlayers(3, [100, 50, 50, 25]));
+
+// 3. leetcode Throtteling Gateway Problem.
+
+function droppedRequests(requestTime) {
+  let count = 0;
+  for (let i = 1; i < requestTime.length; i++) {
+    if (i > 2 && requestTime[i] == requestTime[i - 3]) {
+      count += 1;
+    } else if (i > 19 && (requestTime[i] - requestTime[i - 20]) < 10) {
+      count += 1;
+    } else if (i > 59 && (requestTime[i] - requestTime[i - 60]) < 60) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+// const reqTime = [1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7]
+// console.log(reqTime.length);
+// console.log(droppedRequests(reqTime));
+
+
+
