12f8f888b81eb0793e4626e5db93d6d47fce30da
colewendling <colewendling@utexas.edu>
2021-08-11 10:16:15 -0500

    countTeams algo with combination helper + filter vars

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// 1. short fizzbuzz.
+// Short Fizzbuzz
+
 const fizzBuzz = function (n) {
   for (let i = 1; i <= n; i++) {
     if (i % 15 == 0) console.log('FizzBuzz');
@@ -9,27 +10,27 @@ const fizzBuzz = function (n) {
     else console.log(i);
   }
 }
+
 // console.log(fizzBuzz(15));
 
-// 2. leetcode - scoresArr -> ranks -> # of players pass cutoff rank.
+// scoresArr -> ranks -> # of players pass cutoff rank
 
 function numPlayers(k, scores) {
   const sortedScores = scores.sort(function (a, b) {
     return a - b;
   });
   const ranks = [];
-
   for (let i = 0; i < sortedScores.length; i++) {
     const j = sortedScores.indexOf(sortedScores[i])
     ranks.push(j + 1);
   }
-
   const result = ranks.filter((rank) => rank <= k);
   return result.length;
 }
+
 // console.log(numPlayers(3, [100, 50, 50, 25]));
 
-// 3. leetcode Throtteling Gateway Problem.
+// Throtteling Gateway Problem
 
 function droppedRequests(requestTime) {
   let count = 0;
@@ -48,6 +49,27 @@ function droppedRequests(requestTime) {
 // const reqTime = [1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7]
 // console.log(reqTime.length);
 // console.log(droppedRequests(reqTime));
+
+//4. countTeams combos with filter vars
+
+function countTeams(skills, minPlayers, minLevel, maxLevel) {
+  const sortedSkills = skills.filter(skill => skill <= maxLevel && skill >= minLevel);
+  return combinations(sortedSkills).filter(subArray => subArray.length >= minPlayers).length;
+}
+
+function combinations(array) {
+  return new Array(1 << array.length).fill().map(
+    (e1, i) => array.filter((e2, j) => i & 1 << j));
+}
+
+// const skills = [12, 4, 6, 13, 5, 10];
+// const minPlayers = 3;
+// const minLevel = 4;
+// const maxLevel = 10;
+// console.log(countTeams(skills, minPlayers, minLevel, maxLevel));
+//Expected Output: 5;
+
+
 
 
 
