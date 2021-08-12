fe574ef1e2b753e018ff2a33fbcea4b8f925acc4
colewendling <colewendling@utexas.edu>
2021-08-12 10:18:15 -0500

    add break palindrome problem from leetcode

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -69,6 +69,28 @@ function combinations(array) {
 // console.log(countTeams(skills, minPlayers, minLevel, maxLevel));
 //Expected Output: 5;
 
+// Break Palindrome
+
+function breakPalindrome(palindromeStr) {
+  if (palindromeStr.length === 0) return 'IMPOSSIBLE';
+  let chars = palindromeStr.split('');
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] !== 'a') {
+      if (palindromeStr.length % 2 !== 0 && i === palindromeStr.length / 2) {
+        continue;
+      }
+      chars[i] = 'a';
+      break;
+    }
+    if (i == chars.length - 1) {
+      chars[i] = 'b';
+    }
+  }
+  return chars.join('');
+}
+
+// console.log(breakPalindrome('bab'));
+// Expected Output: 'aab';
 
 
 
