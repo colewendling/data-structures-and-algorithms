350c0eb602788edd3696fbb70d4e90fba37592e5
colewendling <colewendling@utexas.edu>
2021-08-13 10:20:00 -0500

    sock merchant algo that returns pair count from num array

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -92,6 +92,24 @@ function breakPalindrome(palindromeStr) {
 // console.log(breakPalindrome('bab'));
 // Expected Output: 'aab';
 
+// Sock Merchant
+
+function sockMerchant(n, ar) {
+  let socks = {};
+  for (let i = 0; i < n; i++) {
+    socks[ar[i]] ? socks[ar[i]]++ : socks[ar[i]] = 1;
+  }
+  let pairs = 0
+  for (let key in socks) {
+    pairs += Math.floor(socks[key] / 2)
+  }
+  return pairs;
+}
+
+const n = 9;
+const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+// console.log(sockMerchant(n, ar));
 
 
 
