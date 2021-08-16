130c79abc50ef3dd5d916a30eb4967ee943d487e
colewendling <colewendling@utexas.edu>
2021-08-16 10:29:00 -0500

    birthdaycakecandles leetcode

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -166,3 +166,33 @@ function miniMaxSum(arr) {
 // console.log(miniMaxSum(arr1));
 //Output arr: 10 14
 //Output arr1: 299 9271
+
+
+//Birthday Cake Candles Problem
+
+function birthdayCakeCandles(candles) {
+  let sorted = arr.sort(function (a, b) {
+    return a - b;
+  });
+  sorted = sorted.reverse();
+  // console.log(sorted);
+  let count = 0;
+  let current = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (current === 0) {
+      current = sorted[i];
+      count += 1;
+    } else if (current === sorted[i]) {
+      count += 1;
+    } else {
+      return count;
+    }
+  }
+}
+
+const arr = [3, 2, 1, 3];
+const arr1 = new Array(1000).fill(1);
+console.log(birthdayCakeCandles(arr));
+console.log(birthdayCakeCandles(arr1));
+
+// console.log(arr1);
