4c7559fd37352b084041e37904867270364e2927
colewendling <colewendling@utexas.edu>
2021-09-04 16:19:02 -0500

    add common algorithm problem sets

---
+++ 000_DSA/1_common/problems/4_is_subsequence.js
@@ -0,0 +1,20 @@
+//iterative
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+//recursive but not O(1) space
+function isSubsequence(str1, str2) {
+  if (str1.length === 0) return true
+  if (str2.length === 0) return false
+  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+  return isSubsequence(str1, str2.slice(1))
+}
